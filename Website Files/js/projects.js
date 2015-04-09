21579fec98576fa21677af5d63e5999175c781ae
Damien Beard <damienp.beard@gmail.com>
2015-04-09 13:22:43 -0700

    Files Added to repo

---
+++ Website Files/js/projects.js
@@ -0,0 +1,70 @@
+/** Author: Damien Beard, 2015 **/
+
+jQuery(document).ready(function($){
+	
+	var jsonProjects = {};
+	
+	// Load the project data from the generated .json file
+	var load_projects = function(){
+		$.ajax({
+	    	url: "./js/projects.json",
+	    	async: false,
+	    	dataType: 'json',
+	    	success: function(data) {
+	    		jsonProjects = data;
+	    	}
+		});
+	}
+	
+	// For each project, create a thumbnail and add it to the project page. 
+	
+	function show_projects() {
+		var projects_list = "";
+		var viewText = "Visit Site";
+		$.when( load_projects() ).done(function(res1) {
+			for(i=0; i < jsonProjects.projects.length; i++)
+			{
+				//projects_list += '<a class="country_list_item" href="#" inc="'+i+'" dataid="'+jsonProjects.projects[i].ID+'">'+jsonProjects.projects[i].title+'</a>';
+				
+				/* This checks to see if the 'View' button should say something different 
+				   than the default 'View' text. One use case is when it is a document instead
+				   of a webpage */
+				if (jsonProjects.projects[i].viewText) {
+					viewText = jsonProjects.projects[i].viewText
+				} else {
+					// This is a reset in case a previous entry had a different 'View' text.
+					viewText = "Visit Site";
+				}
+				
+				
+				projects_list = '<div class="col-sm-4">\n'+
+				  '<div class="thumbnail" id="'+jsonProjects.projects[i].ID+'_project">\n'+
+					'<a href="project_specific.html?project_id='+jsonProjects.projects[i].ID+'" class="project_image">\n'+
+					  '<img src="images/'+jsonProjects.projects[i].ID+'_thumb.png" alt="'+jsonProjects.projects[i].title+'">\n'+
+					'</a>\n'+
+					'<div class="caption">\n'+
+					  '<h3><a href="project_specific.html?project_id='+jsonProjects.projects[i].ID+'">'+jsonProjects.projects[i].title+' </a></br><small>'+jsonProjects.projects[i].role+'</small></h3>\n'+
+					  '<p>'+jsonProjects.projects[i].blurb+'</p>\n'+
+					  '<p><a href="project_specific.html?project_id='+jsonProjects.projects[i].ID+'" class="btn btn-primary" role="button">Read More</a> <a  href="'+jsonProjects.projects[i].link+'" class="btn btn-default" role="button" target="_blank">'+viewText+'</a></p>\n'+
+					'</div>\n'+
+				  '</div>\n'+
+				'</div>';
+				$('#projects_list').html($('#projects_list').html()+projects_list);
+				
+			}
+			
+			// Make it look pretty with animations
+			$('html,body').animate({
+			  scrollTop: $("#content-main").offset().top
+			}, 1500);
+			//$('#projects_list').html(projects_list);
+			
+			
+		})
+		.fail(function() {
+			console.log( 'I fire if one or more requests failed.' );
+		});
+	}
+	
+	window.show_projects = show_projects;
+});
